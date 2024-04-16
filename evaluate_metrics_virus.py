6ef5dcc8b987d3803d201afc71314c63fc54b806
luise <luise.haeuser@web.de>
2024-04-16 17:22:14 +0200

    Add virus code

---
+++ evaluate_metrics_virus.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+from ete3 import Tree
+import metrics
+import matplotlib.pyplot as plt
+dfs = {}
+for type in ["rsv"]:
+    trees_dir = os.path.join("data/virus", type, "trees", "rooted")
+    tree_names = []
+    for tree_name in os.listdir(trees_dir):
+        tree_file_name = os.path.join(trees_dir, tree_name)
+        if os.path.isfile(tree_file_name):
+            tree_names.append(tree_name)
+    df = pd.DataFrame(tree_names, columns=["tree_name"])
+
+    for i, row in df.iterrows():
+        tree =  Tree(os.path.join(trees_dir, row["tree_name"]))
+        for metric_name in metrics.relative_metrics:
+            df.at[i, metric_name] = metrics.relative(metric_name, tree)
+    print(df)
+    dfs[type] = df
+    df.to_csv(os.path.join("data/virus", type, "metrics.csv"))
